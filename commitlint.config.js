198c0689fb9f9b0192daaa9135f0203377b072ff
yuxino <nbsawz@gmail.com>
2019-10-29 11:03:17 +0800

    :sparkles: feat: new rule of commit message

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,37 @@
-module.exports = { extends: ['@commitlint/config-conventional'] };
+module.exports = {
+  parserPreset: {
+    parserOpts: {
+      headerPattern: /^(.*\w*): (.*)$/,
+      headerCorrespondence: ['type', 'subject']
+    }
+  },
+  rules: {
+    'body-leading-blank': [1, 'always'],
+    'footer-leading-blank': [1, 'always'],
+    'header-max-length': [2, 'always', 72],
+    'scope-case': [2, 'always', 'lower-case'], // ?
+    'subject-empty': [2, 'never'],
+    'subject-full-stop': [2, 'never', '.'],
+    'type-empty': [2, 'never'],
+    'type-case': [2, 'always', 'lower-case'],
+    'type-enum': [
+      2,
+      'always',
+      [
+        '✨ feat',
+        '📦 new',
+        '👌 improve',
+        '🐛 fix',
+        '🚀 release',
+        '🦄 release',
+        '📝 docs',
+        '✅ test',
+        '🔨 refactor',
+        '💩 bad',
+        '🎉 init',
+        '🔥 remove',
+        '🎨 format'
+      ]
+    ]
+  }
+};
